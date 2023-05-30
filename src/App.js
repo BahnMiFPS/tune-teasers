2135762c4c935833a9e7e3e2a16bf5e9e1d952c7
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-05-30 14:03:05 +0700

    Added pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import { ThemeProvider } from "@mui/material";
 import HomePage from "./pages/HomePage";
 import theme from "./theme/theme";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Lobby from "./pages/Lobby";
+import Play from "./pages/Play";
+import Result from "./pages/Result";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/lobby",
+    element: <Lobby />,
+  },
+  {
+    path: "/play",
+    element: <Play />,
+  },
+  {
+    path: "/result",
+    element: <Result />,
+  },
+]);
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <HomePage />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
