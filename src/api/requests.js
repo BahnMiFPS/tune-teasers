24f87a974f5e3b4a61776ad4ab1741bf111ac472
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-06-04 06:45:06 +0700

    deploy to onrender

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,18 +1,18 @@
 const requests = [
   {
     name: "Vietnam",
-    url: `https://tune-teasers.herokuapp.com/api/playlists?country=VN&locale=vi_VN`,
+    url: `https://tune-teaser-server.onrender.com/api/playlists?country=VN&locale=vi_VN`,
     // url: `http://localhost:3001/api/playlists?country=VN&locale=vi_VN`,
   },
   {
     name: "US",
-    url: `https://tune-teasers.herokuapp.com/api/playlists?country=US&locale=en_US`,
+    url: `https://tune-teaser-server.onrender.com/api/playlists?country=US&locale=en_US`,
     // url: `http://localhost:3001/api/playlists?country=US&locale=en_US`,
   },
 
   {
     name: "Kpop",
-    url: `https://tune-teasers.herokuapp.com/api/playlists?country=KR&locale=ko_KR`,
+    url: `https://tune-teaser-server.onrender.com/api/playlists?country=KR&locale=ko_KR`,
     // url: `http://localhost:3001/api/playlists?country=KR&locale=ko_KR`,
   },
 ];
--- src/app/socket.js
+++ src/app/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 const serverAppOrigin =
   process.env.NODE_ENV === "production"
-    ? "https://tune-teasers.herokuapp.com"
+    ? "https://tune-teaser-server.onrender.com"
     : "http://localhost:3001";
 const socket = io.connect(serverAppOrigin);
 export default socket;
