6b88a13147e610bc1d864e1cd907b0102f98fe76
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-06-03 20:06:42 +0700

    s

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,16 +1,16 @@
 const requests = [
   {
     name: "Vietnam",
-    url: `http://localhost:3001/api/playlists?country=VN&locale=vi_VN`,
+    url: `https://tune-teasers.herokuapp.com/api/playlists?country=VN&locale=vi_VN`,
   },
   {
     name: "US",
-    url: `http://localhost:3001/api/playlists?country=US&locale=en_US`,
+    url: `https://tune-teasers.herokuapp.com/api/playlists?country=US&locale=en_US`,
   },
 
   {
     name: "Kpop",
-    url: `http://localhost:3001/api/playlists?country=KR&locale=ko_KR`,
+    url: `https://tune-teasers.herokuapp.com/api/playlists?country=KR&locale=ko_KR`,
   },
 ];
 
